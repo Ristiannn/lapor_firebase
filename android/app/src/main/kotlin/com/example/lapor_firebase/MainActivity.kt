dcc5b768977418386e3de36bbf9b9960445acdc1
Ristian Naufal Apriliawan <ristianapriliawan02@gmail.com>
2023-12-26 07:30:55 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/lapor_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lapor_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lapor_firebase/firebase_options.dart';
+import 'package:lapor_firebase/pages/RegisterPage.dart';
+import 'package:lapor_firebase/pages/SplashPage.dart';
+import 'package:lapor_firebase/pages/dashboard/DashboardPage.dart';
+import 'package:lapor_firebase/pages/LoginPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+      title: 'Lapor Book',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => SplashPage(),
+      '/login': (context) =>  LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      '/dashboard': (context) => const DashboardPage(),
+      // '/add': (context) => AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
+}
