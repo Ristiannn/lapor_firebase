8738eb4ffb578dd0575800e6cd95dfc4a1141b50
Ristian Naufal Apriliawan <ristianapriliawan02@gmail.com>
2024-01-01 15:41:24 +0700

    Second commit

---
+++ lib/components/vars.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+import "package:lapor_firebase/components/styles.dart";
+
+List<String> dataStatus = ['Posted', 'Proses', 'Selesai'];
+List<Color> warnaStaus = [warningColor, dangerColor, successColor];
+List<String> dataInstansi = ['Pembangunan', 'Jalanan', 'Pendidikan'];
